1905411726e44ec47c4396366bb36a1c7dad00af
Lezajic Petar <lezajicpetar@gmail.com>
2025-05-25 09:22:55 +0200

    Prikaz korisnika odredjene grupe preko REST api-ja

---
+++ FRONT/app/UsersGroup/usersGroup.js
@@ -0,0 +1,56 @@
+function Initialize() {
+    let searchButton = document.querySelector('#searchButton')
+    searchButton.addEventListener('click', GetByGroup)
+}
+
+function GetByGroup() {
+    const form = document.querySelector('#groupForm')
+    const data = new FormData(form)
+    const id = data.get('id')
+    
+    fetch(`http://localhost:30471/api/groups/${id}/users`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Rquest failed. Status: ' + response.status)
+            }
+            return response.json()
+        })
+        .then(korisnici => CreateTableRows(korisnici))
+        .catch(error => {
+            console.error('Error:' + error.message)
+        })
+}
+
+function CreateTableRows(userData) {
+    let tableBody = document.querySelector('#userTableBody')
+    tableBody.innerHTML = ''
+
+    if(userData.length === 0) {
+        let tHead = document.querySelector('table thead')
+        tHead.classList.add('hidden')
+
+        let batmanRobin = document.querySelector('#batmanRobin')
+        batmanRobin.classList.remove('hidden')
+    }
+    else{
+        let tHead = document.querySelector('table thead')
+        tHead.classList.remove('hidden')
+
+        let batmanRobin = document.querySelector('#batmanRobin')
+        batmanRobin.classList.add('hidden')
+    }
+
+    userData.forEach(user => {
+        tr = document.createElement('tr')
+        tr.innerHTML = `
+            <td>${user.id}</td>
+            <td>${user.userName}</td>
+            <td>${user.ime}</td>
+            <td>${user.prezime}</td>
+            <td>${user.datumRodjenja}</td>
+        `
+        tableBody.appendChild(tr)
+    })
+}
+
+document.addEventListener('DOMContentLoaded', Initialize)
